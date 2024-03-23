162f300c2e022e121e5f8773d4b8460d8c193c54
Nadilson <nadilson@protonmail.com>
2024-03-23 08:54:07 -0300

    Modal component + Drag & Drop functions

--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -5,6 +5,8 @@ import { Columns } from "../../types";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { Board } from "../../data/board";
 import Task from "../../components/Task";
+import { onDragEnd } from "../../helpers/onDragEnd";
+import AddModal from "../../components/Modals/AdModal";
 
 const Boards = () => {
   const [columns, setColumns] = useState<Columns>(Board);
@@ -27,7 +29,9 @@ const Boards = () => {
 
   return (
     <>
-      <DragDropContext onDragEnd={(result: any) => console.log(result)}>
+      <DragDropContext
+        onDragEnd={(result: any) => onDragEnd(result, columns, setColumns)}
+      >
         <div className="w-full flex items-start justify-between px-5 pb-8 md:gap-0 gap-10">
           {Object.entries(columns).map(([columnId, column]: any) => (
             <div className="w-full flex flex-col gap-0" key={columnId}>
@@ -58,7 +62,10 @@ const Boards = () => {
                   </div>
                 )}
               </Droppable>
-              <div className="flex cursor-pointer items-center justify-center gap-1 py-[10px] md:w-[90%] w-full opacity-90 bg-white rounded-lg shadow-sm text-[#555] font-medium text-[15px]">
+              <div
+                onClick={() => openModal(columnId)}
+                className="flex cursor-pointer items-center justify-center gap-1 py-[10px] md:w-[90%] w-full opacity-90 bg-white rounded-lg shadow-sm text-[#555] font-medium text-[15px]"
+              >
                 <AddOutline color={"#555"} />
                 Add Task
               </div>
@@ -66,6 +73,13 @@ const Boards = () => {
           ))}
         </div>
       </DragDropContext>
+
+      <AddModal
+        isOpen={modalOpen}
+        onClose={closeModal}
+        setOpen={setModalOpen}
+        handleAddTask={handleAddTask}
+      />
     </>
   );
 };
